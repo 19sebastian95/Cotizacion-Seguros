231c2031c13011a40c8d25ad4705946e86416656
Sebastian Ramirez <desarrollador8@azsmart.net>
2020-08-13 18:56:41 -0500

    Updates

---
+++ precache-manifest.4301f1368007ebc956cf8125d51b260e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34236d9562156aaf3999ef9d6dea7ec5",
+    "url": "/Cotizacion-Seguros/index.html"
+  },
+  {
+    "revision": "4fd1a3db781a9fed0494",
+    "url": "/Cotizacion-Seguros/static/css/main.aca52512.chunk.css"
+  },
+  {
+    "revision": "9fccd8313fb1d2915815",
+    "url": "/Cotizacion-Seguros/static/js/2.6dbb9dab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Cotizacion-Seguros/static/js/2.6dbb9dab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fd1a3db781a9fed0494",
+    "url": "/Cotizacion-Seguros/static/js/main.794300fc.chunk.js"
+  },
+  {
+    "revision": "b8c69cd9b27919bbee45",
+    "url": "/Cotizacion-Seguros/static/js/runtime-main.e584ac98.js"
+  }
+]);
